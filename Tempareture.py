ae023a971c85e9c915e1cd0240e689c81c6d04e1
GOVIL K GOWDA <govilkgowda@gmail.com>
2024-06-07 14:32:08 +0530

    Add files via upload

---
+++ Tempareture.py
@@ -0,0 +1,13 @@
+tempareture=int(input('Enter the current tempareture: '))
+
+if tempareture > 35:
+   print('''It's a hot day
+   wear cotten cloths
+   avoid black cloths''')
+elif tempareture > 20:
+   print('''It's normal day
+   wear normal cloths''')
+
+else:
+   print('''It's a cold day
+   wear sweater''')
