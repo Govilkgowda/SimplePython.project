ef5f2ca5116239de205dd7881255f73bf6ba3cc6
GOVIL K GOWDA <govilkgowda@gmail.com>
2024-09-20 19:32:30 +0530

    Update WeightCalculator.py

--- WeightCalculator.py
+++ WeightCalculator.py
@@ -1,12 +1,12 @@
 weight=int(input('Enter your Weight: '))
-unit=input('(L)bs or (k)g: ')
+unit=input('It is in (L)bs or (k)g: ')
 
 if unit.upper()=='L':
    calculate= weight*0.45
    print(f'Your weight is {calculate} Kilos')
 
 elif unit.upper()=='K':
-   calculate= weighr/0.45
+   calculate= weight//0.45
    print(f'Your weight is {calculate} Pounds')
 
 
